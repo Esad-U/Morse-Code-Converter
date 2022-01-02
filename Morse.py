737645d6ae50869999ddd0a0db393e07de254b4f
Esad-U <aesadugur@gmail.com>
2022-01-02 04:04:17 +0300

    sound functions are added to class and called in main

--- Morse.py
+++ Morse.py
@@ -5,6 +5,11 @@ class Morse:
              's': "...", 't': "-", 'u': "..-", 'v': "...-", 'w': ".--",'x': "-..-", 'y': "-.--", 'z': "--..", ' ': "/",
              '.': ".-.-.-"}
 
+    def __init__(self, pin, dot_delay):
+        self.pin = pin
+        self.dot_delay = dot_delay
+        self.dash_delay = dot_delay * 3
+
     def convert_to_morsecode(self, sentence):
         sentence = list(sentence)
         morsecode = []
@@ -13,3 +18,30 @@ class Morse:
             morsecode.append(morse_letter)
         morsecode = " ".join(morsecode)
         return morsecode
+
+    def dot(self):  # Make one dot beep
+        GPIO.output(self.pin, 1)
+        sleep(self.dot_delay)
+        GPIO.output(self.pin, 0)
+
+    def dash(self):  # Make one dash beep
+        GPIO.output(self.pin, 1)
+        sleep(self.dash_delay)
+        GPIO.output(self.pin, 0)
+
+    def convert_and_play(self, sentence):  # Play the converted message
+        output = self.convert_to_morsecode(sentence)
+        print(sentence, " --> ", output)
+        for i in output:
+            if i == '.':
+                self.dot()
+            elif i == '-':
+                self.dash()
+
+            if i == '/':
+                sleep(self.dot_delay)
+            elif i == ' ':
+                sleep(self.dash_delay)
+            else:
+                sleep(self.dot_delay)
+        GPIO.output(self.pin, 0)
--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from Morse import *
+import RPi.GPIO as GPIO
+from time import sleep
 
 if __name__ == '__main__':
-    sentence = input("Enter the sentence to be converted (all lower case, no punctuation): ")
-    converter = Morse()
-    print(converter.convert_to_morsecode(sentence))
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setwarnings(False)
+    pin = 3
+    GPIO.setup(pin, GPIO.OUT)
+    DOT = 0.075  # Constant for duration of one dot in Morse Code
+    cvt = Morse(pin, DOT)
+
+    sentence = input("Enter the sentence to be converted (all lower case, use only '.' as punctuation): ")
+    cvt.convert_and_play(sentence)
